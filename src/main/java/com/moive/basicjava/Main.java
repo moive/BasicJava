274ae06336a4773422afc591eb67d724eb6ddd74
Moises Velasquez <mvelasquez@pictoric.com>
2022-06-22 16:54:36 -0500

    exercise isAdult?

---
+++ src/main/java/com/moive/basicjava/Main.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.moive.basicjava;
+
+/**
+ *
+ * @author USER
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Integer age = 18;
+        Boolean isAdult = age >= 18;
+
+        if (isAdult) {
+            System.out.println("Is adult");
+        }else{
+            System.out.println("Is younger");
+        }
+    }
+
+}
